eb296b4607479432dcb1fa35bad516e279deac93
PatoDckY <jesusf1705dck@gmail.com>
2025-11-29 21:58:09 -0600

    v10

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,7 +1,22 @@
 import { PrismaClient } from '@prisma/client';
 
+const prismaClientSingleton = () => {
+  // 👇 AQUÍ ESTÁ EL TRUCO:
+  // Le decimos: "Usa POSTGRES_PRISMA_URL si existe (Vercel), si no, usa DATABASE_URL (Local)"
+  // También probamos DATABASE_URL_PROD por si acaso la creaste manual.
+  const url = process.env.DATABASE_URL_P || process.env.POSTGRES_PRISMA_URL || process.env.DATABASE_URL;
+
+  return new PrismaClient({
+    datasources: {
+      db: {
+        url: url,
+      },
+    },
+  });
+};
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || prismaClientSingleton();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
